22b8d1e64c1e777c7a7f4293ab3996ace056a4c4
Flooooooooooorian <florian.florianweber.weber@googlemail.com>
2024-05-23 10:22:59 +0200

    Add functions and arrays

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,6 @@ myName = "Marcell"
 console.log(myName)
 
 
-
 //Konstante
 const test: string = "TEST";
 
@@ -32,19 +31,16 @@ let test1: string | undefined = "Florian"
 console.log(test1)
 
 
-
-
-console.log(3+5+age)
+console.log(3 + 5 + age)
 
 console.log(age !== 0.5)
 
 console.log(is18YearsOld && age !== 0.5)
 
 
-if(age !== 0.5) {
+if (age !== 0.5) {
     console.log("Age ist nicht 0.5")
-}
-else {
+} else {
     console.log("Age ist 0.5")
 }
 
@@ -53,11 +49,63 @@ for (let i = 0; i < 10; i++) {
 }
 
 
-
 //Truthy & Falsy
 if (undefined) {
     console.log("true")
-}
-else {
+} else {
     console.log("false")
 }
+
+
+//Funktionen
+
+
+function functionName(param1: string, param2: number): number {
+    console.log(param1)
+    console.log(param2)
+    return param2
+}
+
+const result: number = functionName("test", 123)
+console.log(result)
+
+
+const myFunction = (param1: string, param2: number): string => {
+    console.log(param1)
+    console.log(param2)
+    return param1
+}
+
+
+const result2: string = myFunction("test", 123)
+console.log(result2)
+
+
+//Array & Listen
+
+const myArray: string[] = ["Lukas", "Max"]
+
+console.log(myArray)
+
+console.log(myArray[0])
+
+myArray.push("Elvedin")
+
+console.log(myArray)
+
+myArray.pop()
+
+console.log(myArray)
+
+
+const searchName: string | undefined = myArray.map((name) => name.toUpperCase())
+    .filter(name => name.length > 5)
+    .find(name => name.startsWith("F"))
+
+if (searchName === undefined) {
+    return;
+}
+
+console.log(searchName.length)
+
+console.log(searchName)
